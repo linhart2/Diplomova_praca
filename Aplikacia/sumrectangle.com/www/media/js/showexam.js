6891c475f5cecafb691682f859fb8d29205d9aa4
Daniel Linhart <danko.linhart@gmail.com>
2017-05-11 22:25:11 +0200

    pridanie zobrazenia vytvorenych uloh

--- Aplikacia/sumrectangle.com/www/media/js/showexam.js
+++ Aplikacia/sumrectangle.com/www/media/js/showexam.js
@@ -2,13 +2,39 @@
  * Created by Lingo on 29/04/2017.
  */
 function showExams() {
-    var resultRef = firebase.database().ref('exams/');
+    var resultRef = firebase.database().ref('exams/'+getCookie("teacherID")+"/");
     resultRef.on('child_added', function(data) {
-        $("<li><a>" + data.key + "</a><a class='close' onclick=return PopupAlert('studenta')><i class='fa fa-window-close-o' aria-hidden=true></i></a></li>").appendTo(".GroupList");
-        dict[data.key] = data.val();
-        draw(dict);
+        $("<li id="+data.key+"><a class='open'>" + data.val().examsName + "</a><a class='close' ><i class='fa fa-window-close-o' aria-hidden=true></i></a></li>").appendTo(".GroupList");
+        $("a.close").click({param1: data.key}, deleteExam);
+        $("a.open").click({param1: data.key}, openExam);
     });
 }
+function openExam($examName) {
+    $(".GroupList").empty();
+    var resultRef = firebase.database().ref('exams/'+getCookie("teacherID")+"/"+$examName.data.param1+"/");
+    resultRef.once('value', function(data) {
+        data.forEach(function (snapshot) {
+            console.log(snapshot.key+" "+snapshot.val());
+        })
+        $("<li><a>" + data.key + "</a><a class='close' onclick=return PopupAlert('studenta')><i class='fa fa-window-close-o' aria-hidden=true></i></a><a class='edit' ><i class='fa fa-pencil-square-o' aria-hidden=true></i></a></li>").appendTo(".GroupList");
+    });
+
+}
+
+function deleteExam($key) {
+    if( PopupAlert("cvicenie ?") ) {
+        var ref = firebase.database().ref('exams/'+getCookie("teacherID")+"/"+$key.data.param1);
+        ref.remove(function(error) {
+          alert(error ? "Uh oh!" : "Success!");
+          $("#"+$key.data.param1).remove();
+        });
+    }
+}
+
+function editExam() {
+    console.log("ssss");
+}
+
 $( document ).ready(function() {
     showExams();
 });
--- Aplikacia/sumrectangle.com/www/media/js/sumrectangle.js
+++ Aplikacia/sumrectangle.com/www/media/js/sumrectangle.js
@@ -132,6 +132,22 @@ function removeItem(ref) {
     });
 }
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 function initFirebase() {
     var config = {
         apiKey: "AIzaSyDA8nnfmBPzxZ0pNxjxVObPt_pXOmg_lgM",
