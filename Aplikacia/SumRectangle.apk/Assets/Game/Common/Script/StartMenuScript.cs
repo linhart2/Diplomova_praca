d0452c4ae4a3590b2486b3b2038675f7304a22c7
Daniel Linhart <danko.linhart@gmail.com>
2017-09-09 21:02:33 +0200

    pridanie zabudnuty assetov

---
+++ Aplikacia/SumRectangle.apk/Assets/Game/Common/Script/StartMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenuScript : MonoBehaviour {
+
+	public Button btnOfflineVersion;
+	public Button btnOnlineVersion;
+	// Use this for initialization
+	void Start () {
+		btnOfflineVersion = btnOfflineVersion.GetComponent<Button>();
+		btnOnlineVersion = btnOnlineVersion.GetComponent<Button>();
+	}
+}
---
+++ Aplikacia/SumRectangle.apk/Assets/Game/Online/Script/LoginScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Auth;
+using System;
+
+public class LoginScript : MonoBehaviour {
+
+	public Button btnLogin;
+	public Button btnRegistration;
+	public InputField txtLoginName;
+	public InputField txtLoginPassworld; 
+
+	void Start () {
+		Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+		auth.CreateUserWithEmailAndPasswordAsync ("danko.linhart@gmail.com", "123456").ContinueWith (task => {
+			if(task.IsCanceled){
+				Debug.Log("Canceled");
+				return;
+			}
+			if(task.IsFaulted){
+				Debug.Log("faulted");
+				return;
+			}
+			Firebase.Auth.FirebaseUser newUser = task.Result;
+			Debug.Log(newUser.UserId);
+			if(task.IsCompleted){
+				Debug.Log("Completed");
+			}
+		});
+	}
+}
