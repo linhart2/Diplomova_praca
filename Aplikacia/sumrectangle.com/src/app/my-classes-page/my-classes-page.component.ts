88911847434ed3981339feaeb5800d4da559faf2
Daniel Linhart <danko.linhart@gmail.com>
2018-01-20 22:11:25 +0100

    pridavanie uloh na tabulu

--- Aplikacia/sumrectangle.com/src/app/my-classes-page/my-classes-page.component.ts
+++ Aplikacia/sumrectangle.com/src/app/my-classes-page/my-classes-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../providers/auth.service';
 import {AngularFire, FirebaseListObservable } from 'angularfire2';
+declare var jquery: any;
+declare var $: any;
+
 @Component({
   selector: 'app-my-classes-page',
   templateUrl: './my-classes-page.component.html',
@@ -14,14 +17,21 @@ export class MyClassesPageComponent implements OnInit {
   lName: string;
   isTeacher: boolean;
   uId: string;
+  ulohyNaUlozenie = {};
+  idTriedy;
+  menoTriedy;
 
   private items: FirebaseListObservable<any[]>;
   public zoznamTried: Array<any> = [];
 
   private itemsZoznamUloh: FirebaseListObservable<any[]>;
   public zoznamUloh: Array<any> = [];
+
   public zoznamZobrazPriradeneUlohy: Array<any> = [];
   public zoznamMojichUloh: Array<any> = [];
+
+  nacitajZoznamUlohnaPridanie: boolean;
+  nacitajZoznamPriradenychUloh: boolean;
 
   constructor(private angularFire: AngularFire, public authService: AuthService, private router: Router) {
     this.isLoadedPage = false;
@@ -61,20 +71,50 @@ export class MyClassesPageComponent implements OnInit {
     });
   }
 
+  public pridajPrikladNaTabulu(ev) {
+    if (ev.target.checked) {
+      this.ulohyNaUlozenie[ev.target.defaultValue] = this.zoznamMojichUloh.find(x => x.$key === ev.target.defaultValue).priklady.length;
+    }else {
+      delete this.ulohyNaUlozenie[ev.target.defaultValue];
+    }
+  }
+
+  public ulozPridaneUlohy() {
+    if (this.idTriedy) {
+    this.angularFire.database.object('/TABLES/' + this.idTriedy).update(this.ulohyNaUlozenie);
+    delete this.ulohyNaUlozenie;
+    this.ulohyNaUlozenie = {};
+    delete this.idTriedy;
+    }
+  }
+
+  public zmazPridaneUlohy(examId: string ) {
+    let classID = document.getElementsByClassName('_zobrazUlohy').item(0).getAttribute('id');
+    if (classID === null) {
+      return;
+    }
+    if (this.authService.PopupAlert('ulohu')) {
+      let priklad = this.angularFire.database.object('/TABLES/' + classID + '/' + examId);
+      priklad.remove();
+    }
+  }
+
   public getZoznamMojichUloh() {
     this.itemsZoznamUloh.subscribe(snapshot => {
       let ulohy = [];
       snapshot.forEach( uloha => {
         if (uloha.teacherID === this.uId) {
-            ulohy.push(uloha);
+          ulohy.push(uloha);
         }
       });
       this.zoznamMojichUloh = ulohy;
     });
   }
 
-  public zobrazZoznamPriradenychUloh(classId: string) {
-    let nacitajZoznamPriradenychUloh = true;
+  public zobrazZoznamPriradenychUloh(classId: string, className: string) {
+    this.nacitajZoznamPriradenychUloh = true;
+    this.idTriedy = classId;
+    this.menoTriedy = className;
     let _zoznamUlohNaTabuli = this.angularFire.database.list('/TABLES/' + classId);
     let priradeneUlohy = [];
     _zoznamUlohNaTabuli.subscribe(prikladyNaTabuli => {
@@ -85,30 +125,27 @@ export class MyClassesPageComponent implements OnInit {
           }
         });
       });
-      nacitajZoznamPriradenychUloh = false;
+      this.nacitajZoznamPriradenychUloh = false;
     });
     this.zoznamZobrazPriradeneUlohy = priradeneUlohy;
   }
 
-  public ulozPridaneUlohy(classId: string) {
-
-  }
-
-  public zobrazZoznamUlohNaPridanie(classId: string) {
-    let nacitajZoznamUlohnaPridanie = true;
+  public zobrazZoznamUlohNaPridanie(classId: string, className: string) {
+    this.nacitajZoznamUlohnaPridanie = true;
+    this.idTriedy = classId;
+    this.menoTriedy = className;
     let _zoznamPriradenychUloh = this.angularFire.database.list('/TABLES/' + classId);
-    let zoznamPUloh = this.authService.ConvertToArray(_zoznamPriradenychUloh);
-    this.itemsZoznamUloh.subscribe(snapshot => {
-      let ulohy = [];
-      snapshot.forEach( uloha => {
-        if (uloha.teacherID === this.uId) {
-          if (!zoznamPUloh.some(x => x.$key === uloha.$key)) {
+    _zoznamPriradenychUloh.subscribe(prikladyNaTabuli => {
+      this.itemsZoznamUloh.subscribe(snapshot => {
+        let ulohy = [];
+        snapshot.forEach( uloha => {
+          if ((uloha.teacherID === this.uId) && (!prikladyNaTabuli.some(x => x.$key === uloha.$key))) {
             ulohy.push(uloha);
           }
-        }
-        nacitajZoznamUlohnaPridanie = false;
+          this.nacitajZoznamUlohnaPridanie = false;
+        });
+        this.zoznamUloh = ulohy;
       });
-      this.zoznamUloh = ulohy;
     });
   }
 
